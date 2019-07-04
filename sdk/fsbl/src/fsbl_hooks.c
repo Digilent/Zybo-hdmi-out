2af7e2f31d0de9271dd5e6c6e5b3ac14d6d8bfaa
Ashton Johnson <acj0003@gmail.com>
2019-07-03 23:32:05 -0500

    updated SDK projects to compile in 2018.2. demo tested working.

--- sdk/fsbl/src/fsbl_hooks.c
+++ sdk/fsbl/src/fsbl_hooks.c
@@ -1,43 +1,34 @@
 /******************************************************************************
 *
-* (c) Copyright 2012-2013 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2012 - 2014 Xilinx, Inc.  All rights reserved.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal 
+* in the Software without restriction, including without limitation the rights 
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
 *
-* This file contains confidential and proprietary information of Xilinx, Inc.
-* and is protected under U.S. and international copyright and other
-* intellectual property laws.
+* The above copyright notice and this permission notice shall be included in 
+* all copies or substantial portions of the Software.
 *
-* DISCLAIMER
-* This disclaimer is not a license and does not grant any rights to the
-* materials distributed herewith. Except as otherwise provided in a valid
-* license issued to you by Xilinx, and to the maximum extent permitted by
-* applicable law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL
-* FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS,
-* IMPLIED, OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
-* MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE;
-* and (2) Xilinx shall not be liable (whether in contract or tort, including
-* negligence, or under any other theory of liability) for any loss or damage
-* of any kind or nature related to, arising under or in connection with these
-* materials, including for any direct, or any indirect, special, incidental,
-* or consequential loss or damage (including loss of data, profits, goodwill,
-* or any type of loss or damage suffered as a result of any action brought by
-* a third party) even if such damage or loss was reasonably foreseeable or
-* Xilinx had been advised of the possibility of the same.
+* Use of the Software is limited solely to applications: 
+* (a) running on a Xilinx device, or 
+* (b) that interact with a Xilinx device through a bus or interconnect.  
 *
-* CRITICAL APPLICATIONS
-* Xilinx products are not designed or intended to be fail-safe, or for use in
-* any application requiring fail-safe performance, such as life-support or
-* safety devices or systems, Class III medical devices, nuclear facilities,
-* applications related to the deployment of airbags, or any other applications
-* that could lead to death, personal injury, or severe property or
-* environmental damage (individually and collectively, "Critical
-* Applications"). Customer assumes the sole risk and liability of any use of
-* Xilinx products in Critical Applications, subject only to applicable laws
-* and regulations governing limitations on product liability.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF 
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+* SOFTWARE.
 *
-* THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE
-* AT ALL TIMES.
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in 
+* this Software without prior written authorization from Xilinx.
 *
-*******************************************************************************/
+******************************************************************************/
 
 /*****************************************************************************
 *
@@ -63,9 +54,6 @@
 #include "fsbl.h"
 #include "xstatus.h"
 #include "fsbl_hooks.h"
-#include "xparameters.h"
-#include "xiicps.h"
-#include "xemacps.h"
 
 /************************** Variable Definitions *****************************/
 
@@ -150,79 +138,6 @@ u32 FsblHookBeforeHandoff(void)
 	 * Errors to be stored in the status variable and returned
 	 */
 	fsbl_printf(DEBUG_INFO,"In FsblHookBeforeHandoff function \r\n");
-
-	/* Read Out MAC Address */
-	{
-		int Status;
-		XIicPs Iic;
-		XIicPs_Config *Iic_Config;
-		XEmacPs Emac;
-		XEmacPs_Config *Mac_Config;
-
-		unsigned char mac_addr[6];
-		int i = 0;
-
-		fsbl_printf(DEBUG_GENERAL,"Look Up I2C Configuration\n\r");
-		Iic_Config = XIicPs_LookupConfig(XPAR_PS7_I2C_0_DEVICE_ID);
-		if(Iic_Config == NULL) {
-			return XST_FAILURE;
-		}
-
-		fsbl_printf(DEBUG_GENERAL,"I2C Initialization\n\r");
-		Status = XIicPs_CfgInitialize(&Iic, Iic_Config, Iic_Config->BaseAddress);
-		if(Status != XST_SUCCESS) {
-			return XST_FAILURE;
-		}
-
-		fsbl_printf(DEBUG_GENERAL,"Set I2C Clock\n\r");
-		XIicPs_SetSClk(&Iic, 200000);
-
-		mac_addr[0] = 0xFA;
-
-		fsbl_printf(DEBUG_GENERAL,"Set Memory Read Address\n\r");
-		XIicPs_MasterSendPolled(&Iic, mac_addr, 1, 0x50);
-		while(XIicPs_BusIsBusy(&Iic));
-		fsbl_printf(DEBUG_GENERAL,"Get Mac Address\n\r");
-		XIicPs_MasterRecvPolled(&Iic, mac_addr, 6, 0x50);
-		while(XIicPs_BusIsBusy(&Iic));
-
-		fsbl_printf(DEBUG_GENERAL,"MAC Addr: ");
-		for(i = 0; i < 6; i++) {
-			fsbl_printf(DEBUG_GENERAL,"%02x ", mac_addr[i]);
-		}
-		fsbl_printf(DEBUG_GENERAL,"\n\r");
-
-		fsbl_printf(DEBUG_GENERAL,"Look Up Emac Configuration\n\r");
-		Mac_Config = XEmacPs_LookupConfig(XPAR_PS7_ETHERNET_0_DEVICE_ID);
-		if(Mac_Config == NULL) {
-			return XST_FAILURE;
-		}
-
-		fsbl_printf(DEBUG_GENERAL,"Emac Initialization\n\r");
-		Status = XEmacPs_CfgInitialize(&Emac, Mac_Config, Mac_Config->BaseAddress);
-		if(Status != XST_SUCCESS){
-			return XST_FAILURE;
-		}
-
-		fsbl_printf(DEBUG_GENERAL,"Set Emac MAC Address\n\r");
-		Status = XEmacPs_SetMacAddress(&Emac, mac_addr, 1);
-		if(Status != XST_SUCCESS){
-			return XST_FAILURE;
-		}
-
-		fsbl_printf(DEBUG_GENERAL,"Verify Emac MAC Address\n\r");
-		XEmacPs_GetMacAddress(&Emac, mac_addr, 1);
-		if(Status != XST_SUCCESS){
-			return XST_FAILURE;
-		}
-
-		xil_printf("MAC Addr: ");
-		for(i = 0; i < 6; i++) {
-			xil_printf("%02x ", mac_addr[i]);
-		}
-		xil_printf("\n\r");
-	}
-
 
 	return (Status);
 }
